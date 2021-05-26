83b8f3538d16b083fdef29f8d7cddc4f6546b116
Manish Kumar <79128476+SirManishKumar@users.noreply.github.com>
2021-05-26 13:06:21 +0530

    Update .4.sh

--- .4.sh
+++ .4.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #version 1.0
 clear
+pkg install curl
+pkg install wget
+pkg install git
+pip install lolcat
+pkg install figlet
+clear
 green="\e[1;32m"
 yellow="\033[33;1m"
 indigo="\033[34;1m"
@@ -9,14 +15,14 @@ purple="\033[37;1m"
 cyan="\033[36;1m"
 white="\033[39;1m"
 
-printf "\e[1;32m\t+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\e[0m\n"
-printf "\e[1;37m\t      |A|l|l|T|o|o|l|s|   \e[0m\n"
-printf "\e[1;32m\t+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\e[0m\n"
+printf "\e[1;32m\t    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\e[0m\n"
+printf "\e[1;37m\t               |M|N|S|A|l|l|T|o|o|l|s|   \e[0m\n"
+printf "\e[1;32m\t    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\e[0m\n"
 printf "\n"
-printf "\e[1;40m\tCreated by:\e[0m\e[1;42mSirManishKumar\e[0m\n"
+printf "\e[1;40m\t            Created by:\e[0m\e[1;42mSirManishKumar\e[0m\n"
 printf "\n"
 
-printf "\e[1;93m==============[\e[0m\e[1;95mAll Tools Installer]\e[0m\e[1;93m===============\e[0m\n"
+printf "\e[1;93m====================[\e[0m\e[1;95mAll Tools Installer]\e[0m\e[1;93m=====================\e[0m\n"
 sleep 1
 ###################################################
 # CTRL + C
@@ -25,7 +31,7 @@ trap ctrl_c INT
 ctrl_c() {
 clear
 echo "Author : Manish Kumar Chaturvedi" | lolcat
-figlet -f slant "AllTools" | lolcat
+figlet -f slant "MNSAllTools" | lolcat
 sleep 1
 exit
 }
@@ -33,6 +39,11 @@ exit
 lagi=1
 while [ $lagi -lt 6 ];
 do
+figlet -f slant "MNSAllTools" | lolcat
+echo "╭━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━╮" | lolcat
+echo "   Created By:- https://github.com/SirManishKumar/TermuxLock " | lolcat
+echo "     Follow On Twitter:- http://twtter.com/SirManishKumar " | lolcat
+echo "╰━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━╯" | lolcat
 echo -e "\e[1;36m "
 echo "314. Install Recon	 333. Install maskphish"
 echo '315. Install Nexphisher    334. Install Anonphisher'
@@ -57,7 +68,7 @@ echo "===================================================" | lolcat
 echo "            00. Exit            N. Next" | lolcat
 echo "===================================================" | lolcat
 echo -e "\e[1;32m╭─[ Select Tool to Install ]\e[0m"
-read -p "╰─> " pil;
+read -p "╰──> " pil;
 
 case $pil in
 00) echo "Author : Manish Kumar Chaturvedi" | lolcat

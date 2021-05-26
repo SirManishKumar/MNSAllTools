9715835a84be26cbf851364d538be4f0af83b17e
Manish Kumar <79128476+SirManishKumar@users.noreply.github.com>
2021-05-26 11:42:47 +0530

    Update termux-install.sh

--- termux-install.sh
+++ termux-install.sh
@@ -40,16 +40,16 @@ lagi=1
 while [ $lagi -lt 6 ];
 do
 echo -e "\e[1;36m"
-echo "                              __" | lolcat
-echo "          ____  _            |  |        __  __ _   _  ____" | lolcat
-echo "         / ___|(_)_ __    ___|  |___    |  \/  | \ | |/ ___|" | lolcat
-echo "         \___ \| | `__|   \__    __/    | |\/| |  \| |\___ \" | lolcat
-echo "          ___) | | |         \  /       | |  | | |\  | ___) |" | lolcat
-echo "  __  __ |____/|_|_| _     _  \/ _  __  |_|  |_|_| \_||____/" | lolcat
-echo " |  \/  | __ _ _ __ (_)___| |__ | |/ /   _ _ __ ___   __ _ _ _" | lolcat
-echo " | |\/| |/ _` | `_ \| / __| `_ \| ` / | | | `_ ` _ \ / _` | `_|" | lolcat
-echo " | |  | | (_| | | | | \__ \ | | | . \ |_| | | | | | | (_| | |" | lolcat
-echo " |_|  |_|\__,_|_| |_|_|___/_| |_|_|\_\__,_|_| |_| |_|\__,_|_|" | lolcat
+echo "                              __ " | lolcat
+echo "          ____  _            |  |        __  __ _   _  ____ " | lolcat
+echo "         / ___|(_)_ __    ___|  |___    |  \/  | \ | |/ ___| " | lolcat
+echo "         \___ \| | `__|   \__    __/    | |\/| |  \| |\___ \ " | lolcat
+echo "          ___) | | |         \  /       | |  | | |\  | ___) | " | lolcat
+echo "  __  __ |____/|_|_| _     _  \/ _  __  |_|  |_|_| \_||____/ " | lolcat
+echo " |  \/  | __ _ _ __ (_)___| |__ | |/ /   _ _ __ ___   __ _ _ _ " | lolcat
+echo " | |\/| |/ _` | `_ \| / __| `_ \| ` / | | | `_ ` _ \ / _` | `_| " | lolcat
+echo " | |  | | (_| | | | | \__ \ | | | . \ |_| | | | | | | (_| | | " | lolcat
+echo " |_|  |_|\__,_|_| |_|_|___/_| |_|_|\_\__,_|_| |_| |_|\__,_|_| " | lolcat
 echo "╭━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━╮" | lolcat
 echo "   Created By:- https://github.com/SirManishKumar/TermuxLock " | lolcat
 echo "     Follow On Twitter:- http://twtter.com/SirManishKumar " | lolcat

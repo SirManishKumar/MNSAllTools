b16253795c843a41a02768b632983ea06a44986b
Manish Kumar <79128476+SirManishKumar@users.noreply.github.com>
2021-05-26 11:53:51 +0530

    Update termux-install.sh

--- termux-install.sh
+++ termux-install.sh
@@ -39,27 +39,18 @@ exit
 lagi=1
 while [ $lagi -lt 6 ];
 do
-echo -e "\e[1;36m"
-echo "                              __ " | lolcat
-echo "          ____  _            |  |        __  __ _   _  ____ " | lolcat
-echo "         / ___|(_)_ __    ___|  |___    |  \/  | \ | |/ ___| " | lolcat
-echo "         \___ \| | `__|   \__    __/    | |\/| |  \| |\___ \ " | lolcat
-echo "          ___) | | |         \  /       | |  | | |\  | ___) | " | lolcat
-echo "  __  __ |____/|_|_| _     _  \/ _  __  |_|  |_|_| \_||____/ " | lolcat
-echo " |  \/  | __ _ _ __ (_)___| |__ | |/ /   _ _ __ ___   __ _ _ _ " | lolcat
-echo " | |\/| |/ _` | `_ \| / __| `_ \| ` / | | | `_ ` _ \ / _` | `_| " | lolcat
-echo " | |  | | (_| | | | | \__ \ | | | . \ |_| | | | | | | (_| | | " | lolcat
-echo " |_|  |_|\__,_|_| |_|_|___/_| |_|_|\_\__,_|_| |_| |_|\__,_|_| " | lolcat
+figlet -f slant "AllTools" | lolcat
 echo "╭━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━╮" | lolcat
 echo "   Created By:- https://github.com/SirManishKumar/TermuxLock " | lolcat
 echo "     Follow On Twitter:- http://twtter.com/SirManishKumar " | lolcat
 echo "╰━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━╯" | lolcat
+echo -e "\e[1;36m"
 echo ' 1. Install For Termux'
 echo ' 2. Install For Linux'
 echo ' 3. Install For Window'
-echo "========================================================" | lolcat
-echo "            Please Choose Your Operating System" | lolcat
-echo "========================================================" | lolcat
+echo "===========================================================" | lolcat
+echo "             Please Choose Your Operating System" | lolcat
+echo "===========================================================" | lolcat
 echo -e "\e[1;32m╭─[ Select number to install ]"
 read -p "╰─> " pil;
 case $pil in
